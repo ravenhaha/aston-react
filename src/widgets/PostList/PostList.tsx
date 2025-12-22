a3e58578ae87d5417467fa9259eb368702526cd9
ravenhaha <kirillbazhenov22@Gmail.com>
2025-12-22 16:02:37 +0300

    add homework2 Задание: 2. Использовать React.Fragment и key в списках.

--- src/widgets/PostList/PostList.tsx
+++ src/widgets/PostList/PostList.tsx
@@ -1,21 +1,23 @@
 import { PostCard } from '../../entities/post/ui/PostCard'
+import { Fragment } from 'react';
 
 const posts = [
     { id: 1, title: 'Post 1', body: 'Body 1' },
     { id: 2, title: 'Post 2', body: 'Body 2' },
-]
+];
 
 export const PostList = () => {
     return (
         <div>
             {posts.map(post => (
-                <PostCard
-                    key={post.id}
-                    title={post.title}
-                    body={post.body}
-                />
+                <Fragment key={post.id}>
+                    <PostCard
+                        title={post.title}
+                        body={post.body}
+                    />
+                </Fragment>
             ))}
         </div>
-    )
-}
+    );
+};
 
